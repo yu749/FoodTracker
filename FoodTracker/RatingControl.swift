85a7144692e729fe87f72c404672db3ecd69328d
yu <11111@gmail.com>
2021-03-23 23:48:06 +0900

    評価ボタンを追加。ボタンのサイズと数をIBInspectableで書き換え可能にした。

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -7,16 +7,70 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+     
+    var rating = 0
 
     //MARK: Initialization
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
 
 }
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -36,10 +36,10 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         // Dismiss the picker if the user canceled.
         dismiss(animated: true, completion: nil)
     }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         // The info dictionary may contain multiple representations of the image. You want to use the original.
-        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage else {
+        guard let selectedImage = info[.originalImage] as? UIImage else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
         }
         
