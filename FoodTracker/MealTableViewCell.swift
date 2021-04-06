4372d457e9c8f83fd9447c3258a32ff4784b3ce0
yu <11111@gmail.com>
2021-04-06 23:39:33 +0900

    tableviewの追加

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Tanimura Yudai on 2021/04/05.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
